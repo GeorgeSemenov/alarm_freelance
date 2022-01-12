a86cb9f45196641723aa2701b3a452d7b23e0c68
GeorgeSemenov <zhora.sem@yandex.ru>
2022-01-12 17:35:03 +0300

    add some functions

--- freelancehunt parser2.user.js
+++ freelancehunt parser2.user.js
@@ -12,5 +12,13 @@
 (function() {
     'use strict';
     alert("i'm pare freelancehunt");
-    // Your code here...
+    let projects = $('table > tbody > tr');/*массив всех проектов(несортированный массив)*/
+    let premium_projects = [];/*массив премиальных проектов*/
+    let common_projects =[]; /*массив не премиальных проектов*/
+
+    for (i=0; i < projects.length; i++){/*пробегаемся по всем проектам, и складываем их по разным массивам*/
+        let project = projects[i];
+        if project.chil
+    }
+    projects.css({'background-color':'red'});
 })();
