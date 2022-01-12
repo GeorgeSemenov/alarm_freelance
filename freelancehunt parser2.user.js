9e1e7a64244cc7a3928e2bfe9581d1956594a2ef
GeorgeSemenov <zhora.sem@yandex.ru>
2022-01-12 10:48:21 +0300

    first blood

---
+++ freelancehunt parser2.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         freelancehunt parse
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Зачем он нуже -не знает никто
+// @author       Гошанбульйон
+// @match        https://freelancehunt.com/*
+// @icon         https://freelancehunt.com/static/images/apple-icon-57x57.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert("i'm pare freelancehunt");
+    // Your code here...
+})();
